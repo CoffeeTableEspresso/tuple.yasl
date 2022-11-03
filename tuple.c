1c0e7db0db100f8fadc4653cb005ede6eb84ee89
CoffeeTableEspresso <coffeetableespresso@gmail.com>
2022-11-03 00:31:25 -0400

    fix compile

--- tuple.c
+++ tuple.c
@@ -12,7 +12,7 @@ struct YASL_Tuple {
 	struct YASL_Object items[];
 };
 
-void tuple_free(struct YASL_Tuple *tuple) {
+void tuple_free(struct YASL_State *S, struct YASL_Tuple *tuple) {
 	for (size_t i = 0; i < tuple->len; i++) {
 		dec_ref(tuple->items + i);
 	}
@@ -26,7 +26,7 @@ static struct YASL_Tuple *tuple_alloc(size_t len) {
 }
 
 static void YASL_pushtuple(struct YASL_State *S, struct YASL_Tuple *tuple) {
-	YASL_pushuserdata(S, tuple, TUPLE_NAME, (void (*)(void *))tuple_free);
+	YASL_pushuserdata(S, tuple, TUPLE_NAME, (void (*)(struct YASL_State *, void *))tuple_free);
 	YASL_loadmt(S, TUPLE_PRE);
 	YASL_setmt(S);
 }
